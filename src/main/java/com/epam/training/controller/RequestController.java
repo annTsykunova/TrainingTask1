ebddd3911e8d9243305e838692bb6e3f3bfec838
Hanna Tsykunova <Hanna_Tsykunova@epam.com>
2017-03-08 23:22:55 +0300

    add post and get methods, change log4j.properties

--- src/main/java/com/epam/training/controller/RequestController.java
+++ src/main/java/com/epam/training/controller/RequestController.java
@@ -2,10 +2,12 @@ package com.epam.training.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Calendar;
 
 @RestController
 @RequestMapping("/data")
@@ -13,17 +15,23 @@ public class RequestController {
 
     private static final Logger logger = Logger.getLogger(RequestController.class);
 
-    @RequestMapping("/request")
-    public String getDataRequest(@RequestParam(value = "id")Integer id){
+    @RequestMapping(value = "/request", method = RequestMethod.GET)
+    public String getDataRequest(@RequestParam(value = "id") Integer id) {
         logger.info(" Request param:" + id);
         return "Request param:" + id;
     }
 
-    @RequestMapping("url")
-    public String getURLRequest(HttpServletRequest request){
+    @RequestMapping(value = "url", method = RequestMethod.GET)
+    public String getURLRequest(HttpServletRequest request) {
         String test = request.getRequestURI();
         logger.info("URL:" + test);
         return test;
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public String postData() {
+        String data = Calendar.getInstance().getCalendarType() + "it's answer on POST!";
+        logger.info("Post:" + data);
+        return data;
+    }
 }

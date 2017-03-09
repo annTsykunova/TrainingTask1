dac28bee7f8650a854fea9084b0f374256ef2275
annTsykunova <tsykunovaane4ka@gmail.com>
2017-03-09 14:32:41 +0300

    Update DataController.java

--- src/main/java/com/epam/training/controller/DataController.java
+++ src/main/java/com/epam/training/controller/DataController.java
@@ -13,25 +13,25 @@ import java.util.Calendar;
 @RequestMapping("/data")
 public class DataController {
 
-    private static Logger logger = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @RequestMapping(value = "/request", method = RequestMethod.GET)
     public String getDataRequest(@RequestParam("id") Long id) {
-        logger.info(" Request param:" + id);
+        LOGGER.info(" Request param:" + id);
         return "Request param:" + id;
     }
 
     @RequestMapping(value = "/url", method = RequestMethod.GET)
     public String getURLRequest(HttpServletRequest request) {
         String test = request.getRequestURI();
-        logger.info("URL:" + test);
+        LOGGER.info("URL:" + test);
         return test;
     }
 
     @RequestMapping(value = "/user/{id:[\\d]+}",method = RequestMethod.POST)
     public String postData(@PathVariable("id") Long id) {
         String data = Calendar.getInstance().getCalendarType() + "it's answer on POST with id = " + id;
-        logger.info("Post:" + data);
+        LOGGER.info("Post:" + data);
         return data;
     }
 }

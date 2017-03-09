caa3a7e1b5a6d4b3dcb4dc845d17d60761a96989
Hanna Tsykunova <Hanna_Tsykunova@epam.com>
2017-03-09 11:22:00 +0300

    change log4j 1 into log4j 2

--- src/main/java/com/epam/training/controller/RequestController.java
+++ src/main/java/com/epam/training/controller/RequestController.java
@@ -1,6 +1,7 @@
 package com.epam.training.controller;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Calendar;
 
+
+
 @RestController
 @RequestMapping("/data")
 public class RequestController {
 
-    private static final Logger logger = Logger.getLogger(RequestController.class);
+    private static Logger logger = LogManager.getLogger();
 
     @RequestMapping(value = "/request", method = RequestMethod.GET)
     public String getDataRequest(@RequestParam(value = "id") Integer id) {
@@ -21,7 +24,7 @@ public class RequestController {
         return "Request param:" + id;
     }
 
-    @RequestMapping(value = "url", method = RequestMethod.GET)
+    @RequestMapping(value = "/url", method = RequestMethod.GET)
     public String getURLRequest(HttpServletRequest request) {
         String test = request.getRequestURI();
         logger.info("URL:" + test);
